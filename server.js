ef1a4208875417de4a7f1b3aae7900b48cf284ea
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-31 14:21:21 -0700

    add student functionality from lab today added

--- server.js
+++ server.js
@@ -34,4 +34,26 @@ app.get('/js', (req, res) => {
 	}
 });
 
+app.post('/api/student', (req, res) => {
+	let { name } = req.body;
+	name = name.trim();
+
+	const index = students.findIndex((studentName) => studentName === name);
+
+	if (index === -1 && name !== '') {
+		students.push(name);
+		rollbar.log('Student added successfully', {
+			author: 'Scott',
+			type: 'manual entry',
+		});
+		res.status(200).send(students);
+	} else if (name === '') {
+		rollbar.critical('No name given');
+		res.status(400).send('must provide a name.');
+	} else {
+		rollbar.error('student already exists');
+		res.status(400).send('that student already exists');
+	}
+});
+
 app.listen(port, () => console.log(`take us to warp ${port}`));
