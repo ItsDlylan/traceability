b4b6a3e55d6f811f3199947eecae73682253975d
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-31 14:34:40 -0700

    trying import

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios').default;
+import axios from 'axios';
 const addForm = document.querySelector('form');
 const nameInput = document.querySelector('input');
 const container = document.querySelector('section');
